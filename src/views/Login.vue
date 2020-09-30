081471730c21c1a36e73ce3e813a9e9081828b7a
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-09-30 16:20:50 -0600

    Added signup and login page and forms

---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+ <h1> THIS IS THE LOGIN </h1>
+ <p> Email </p>
+ <input type="text" id="email-input" v-model="email">
+ <p> Password </p>
+ <input type="password" id="password-input" v-model="password">
+ <h2 @click="loginUser"> Login </h2>
+ <h3> {{ loginStatus }} </h3>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import cookies from 'vue-cookies'
+
+    export default {
+        data() {
+            return {
+              email: "",
+              password: "",
+              loginStatus: "",
+            }
+        },
+        methods: {
+           
+            loginUser: function() {
+                this.loginStatus = "Loading"
+                axios.request({
+                url: "https://tweeterest.ml/api/login",
+                method: "POST",
+                headers: {
+                "Content-Type":"application/json",
+                "X-Api-Key":"SVzuhkqP5JrStTsfETYXW6UQZs0UV95ENy1VscJoZ3L5P",
+                },
+                data: {
+                    email: this.email,
+                    password: this.password
+                }
+                }).then((response) => {
+                    //CHECK IF LOGIN TOKEN SENT
+                console.log(response)
+                this.loginStatus = "Success"
+                cookies.set('session', response.data.loginToken)
+                // SEND USER TO HOME PAGE
+                }).catch((error) => {
+                    //SHOW USER LOGIN FAILURE
+                console.log(error)
+                this.loginStatus = "Error"
+                })
+            }
+        },
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+<router-link to="/login">Login</router-link>
+<h1> Sign Up Page </h1>
+<signup-form> </signup-form>
+    </div>
+</template>
+
+<script>
+import SignupForm from "../components/SignupForm.vue"
+
+    export default {
+        name: "signup-page",
+
+       components: {
+           SignupForm,
+       },
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
